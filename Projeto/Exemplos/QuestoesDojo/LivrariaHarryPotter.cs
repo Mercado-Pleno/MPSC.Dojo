9899c9628ed51193695b653721e58db48b537b39
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2016-05-09 14:23:36 +0000

    Harry Potter

---
+++ Projeto/Exemplos/QuestoesDojo/LivrariaHarryPotter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MPSC.Library.Exemplos.QuestoesDojo
+{
+	public class LivrariaHarryPotter
+	{
+		private const Decimal valorCadaLivro = 42.00M;
+
+
+		public Decimal ValorAPagar(params IntencaoDeCompra[] titulos)
+		{
+			var titulosDesagrupados = DesagruparTitulos(titulos);
+			var quantidadeDiferente = titulosDesagrupados.Distinct(IntencaoDeCompra.Comparador.Instancia).Count();
+
+			var valorTotal = 0.00M;
+			foreach (var titulo in titulosDesagrupados)
+			{
+				valorTotal += valorCadaLivro * titulo.Quantidade;
+			}
+
+			var fatorDeDesconto = ObterFatorDeDesconto(quantidadeDiferente);
+
+			return valorTotal * fatorDeDesconto;
+		}
+
+		public IntencaoDeCompra[] DesagruparTitulos(params IntencaoDeCompra[] titulos)
+		{
+			return titulos
+				.SelectMany(t => Enumerable.Repeat(new IntencaoDeCompra { Livro = t.Livro, Quantidade = 1 }, t.Quantidade))
+				.ToArray();
+		}
+
+		public IntencaoDeCompra[] AgruparTitulos(params IntencaoDeCompra[] titulos)
+		{
+			return titulos
+				.GroupBy(t => t.Livro)
+				.Select(g => new IntencaoDeCompra { Livro = g.Key, Quantidade = g.Sum(t => t.Quantidade) })
+				.ToArray();
+		}
+
+		public Decimal ObterFatorDeDesconto(Int32 quantidadeDeLivrosNoPacote)
+		{
+			if (quantidadeDeLivrosNoPacote <= 0)
+				throw new Exception("Informe uma quantidade Válida");
+			else if (quantidadeDeLivrosNoPacote == 1)
+				return 1.00M;
+			else if (quantidadeDeLivrosNoPacote == 2)
+				return 0.95M;
+			else if (quantidadeDeLivrosNoPacote == 3)
+				return 0.90M;
+			else if (quantidadeDeLivrosNoPacote == 4)
+				return 0.85M;
+			else
+				return 0.80M;
+		}
+	}
+
+	public class IntencaoDeCompra
+	{
+		public String Livro { get; set; }
+		public Int32 Quantidade { get; set; }
+
+		public class Comparador : IEqualityComparer<IntencaoDeCompra>
+		{
+			public static readonly Comparador Instancia = new Comparador();
+			private Comparador() { }
+			public Boolean Equals(IntencaoDeCompra x, IntencaoDeCompra y)
+			{
+				return x.Livro.ToUpper().Equals(y.Livro.ToUpper());
+			}
+
+			public Int32 GetHashCode(IntencaoDeCompra obj)
+			{
+				return obj.Livro.GetHashCode();
+			}
+		}
+	}
+}
---
+++ Projeto/[TestesUnitarios]/SolutionTest_v4.0/Exemplos/QuestoesDojo/TestandoLivrariaHarryPotter.cs
@@ -0,0 +1,289 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPSC.Library.Exemplos.QuestoesDojo;
+using System;
+using System.Linq;
+
+namespace MP.Library.TestesUnitarios.SolutionTest_v4.Exemplos.QuestoesDojo
+{
+	[TestClass]
+	public class TestandoLivrariaHarryPotter
+	{
+		[TestMethod]
+		public void QuandoAgrupa1Livro1Titulos()
+		{
+			var livraria = new LivrariaHarryPotter();
+			var agrupado = livraria.AgruparTitulos(new IntencaoDeCompra { Livro = "A", Quantidade = 1 });
+			var desagrupado = livraria.DesagruparTitulos(agrupado);
+
+			Assert.AreEqual(1, agrupado.Length);
+			Assert.AreEqual("A", agrupado[0].Livro);
+			Assert.AreEqual(1, agrupado[0].Quantidade);
+
+			Assert.IsTrue(desagrupado.All(t => t.Quantidade == 1));
+			Assert.AreEqual(1, desagrupado.Length);
+			Assert.AreEqual("A", desagrupado[0].Livro);
+			Assert.IsTrue(desagrupado.All(t => t.Livro == "A"));
+			Assert.AreEqual("A", desagrupado.Distinct().Single().Livro);
+		}
+
+		[TestMethod]
+		public void QuandoAgrupa2Livros1Titulos()
+		{
+			var livraria = new LivrariaHarryPotter();
+			var agrupado = livraria.AgruparTitulos(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "A", Quantidade = 5 });
+			var desagrupado = livraria.DesagruparTitulos(agrupado);
+
+			Assert.AreEqual(1, agrupado.Length);
+			Assert.AreEqual("A", agrupado[0].Livro);
+			Assert.AreEqual(6, agrupado[0].Quantidade);
+
+			Assert.IsTrue(desagrupado.All(t => t.Quantidade == 1));
+			Assert.AreEqual(6, desagrupado.Length);
+			Assert.IsTrue(desagrupado.All(t => t.Livro == "A"));
+			Assert.AreEqual("A", desagrupado.Distinct().Single().Livro);
+		}
+
+		[TestMethod]
+		public void QuandoAgrupa5Livros1Titulos()
+		{
+			var livraria = new LivrariaHarryPotter();
+			var agrupado = livraria.AgruparTitulos(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 2 },
+				new IntencaoDeCompra { Livro = "A", Quantidade = 3 },
+				new IntencaoDeCompra { Livro = "A", Quantidade = 5 },
+				new IntencaoDeCompra { Livro = "A", Quantidade = 8 },
+				new IntencaoDeCompra { Livro = "A", Quantidade = 13 });
+			var desagrupado = livraria.DesagruparTitulos(agrupado);
+
+			Assert.AreEqual(1, agrupado.Length);
+			Assert.AreEqual("A", agrupado[0].Livro);
+			Assert.AreEqual(31, agrupado[0].Quantidade);
+
+			Assert.IsTrue(desagrupado.All(t => t.Quantidade == 1));
+			Assert.AreEqual(31, desagrupado.Length);
+			Assert.IsTrue(desagrupado.All(t => t.Livro == "A"));
+			Assert.AreEqual("A", desagrupado.Distinct().Single().Livro);
+		}
+
+		[TestMethod]
+		public void QuandoAgrupa5Livros2Titulos()
+		{
+			var livraria = new LivrariaHarryPotter();
+			var agrupado = livraria.AgruparTitulos(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 2 },
+				new IntencaoDeCompra { Livro = "A", Quantidade = 3 },
+				new IntencaoDeCompra { Livro = "A", Quantidade = 5 },
+				new IntencaoDeCompra { Livro = "A", Quantidade = 8 },
+				new IntencaoDeCompra { Livro = "A", Quantidade = 13 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 21 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 34 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 55 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 89 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 144 });
+			var desagrupado = livraria.DesagruparTitulos(agrupado);
+
+			Assert.AreEqual(2, agrupado.Length);
+			Assert.AreEqual("A", agrupado[0].Livro);
+			Assert.AreEqual(31, agrupado[0].Quantidade);
+			Assert.AreEqual("B", agrupado[1].Livro);
+			Assert.AreEqual(343, agrupado[1].Quantidade);
+
+			Assert.IsTrue(desagrupado.All(t => t.Quantidade == 1));
+			Assert.AreEqual(374, desagrupado.Length);
+			Assert.IsTrue(desagrupado.Any(t => t.Livro == "A"));
+			Assert.IsTrue(desagrupado.Any(t => t.Livro == "A"));
+			Assert.AreEqual("A", desagrupado.Distinct().First().Livro);
+			Assert.AreEqual("B", desagrupado.Distinct().Last().Livro);
+		}
+
+
+		[TestMethod]
+		public void QuandoCompraApenasUmLivro()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(1.0M, livraria.ObterFatorDeDesconto(1));
+		}
+
+		[TestMethod]
+		public void QuandoCompraDoisLivrosDeTitulosDiferentes()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(0.95M, livraria.ObterFatorDeDesconto(2));
+		}
+
+		[TestMethod]
+		public void QuandoCompraTresLivrosDeTitulosDiferentes()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(0.90M, livraria.ObterFatorDeDesconto(3));
+		}
+
+		[TestMethod]
+		public void QuandoCompraQuatroLivrosDeTitulosDiferentes()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(0.85M, livraria.ObterFatorDeDesconto(4));
+		}
+
+		[TestMethod]
+		public void QuandoCompraCincoLivrosDeTitulosDiferentes()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(0.80M, livraria.ObterFatorDeDesconto(5));
+		}
+
+		[TestMethod]
+		public void QuandoCompraMaisDeCincoLivrosDeTitulosDiferentes()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(0.80M, livraria.ObterFatorDeDesconto(6));
+			Assert.AreEqual(0.80M, livraria.ObterFatorDeDesconto(7));
+			Assert.AreEqual(0.80M, livraria.ObterFatorDeDesconto(8));
+			Assert.AreEqual(0.80M, livraria.ObterFatorDeDesconto(100));
+			Assert.AreEqual(0.80M, livraria.ObterFatorDeDesconto(1000));
+		}
+
+		[TestMethod, ExpectedException(typeof(Exception))]
+		public void QuandoCompraUmaQuantidadeIgualAZeroDeveLancarExcecao()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(0.80M, livraria.ObterFatorDeDesconto(0));
+			Assert.Fail("Deveria Lancar Excecao e não lancou");
+		}
+
+		[TestMethod, ExpectedException(typeof(Exception))]
+		public void QuandoCompraUmaQuantidadeMenorQueZeroDeveLancarExcecao()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(0.80M, livraria.ObterFatorDeDesconto(-1));
+			Assert.Fail("Deveria Lancar Excecao e não lancou");
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraApenasUmLivro()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(42.00M, livraria.ValorAPagar(new IntencaoDeCompra { Livro = "A", Quantidade = 1 }));
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraDeDoisLivrosDoMesmoTituloManeira1()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(84.00M, livraria.ValorAPagar(new IntencaoDeCompra { Livro = "A", Quantidade = 2 }));
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraDeDoisLivrosDoMesmoTituloManeira2()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(84.00M, livraria.ValorAPagar(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "A", Quantidade = 1 }));
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraDeDoisLivrosDeTitulosDiferentes()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(79.80M, livraria.ValorAPagar(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 1 }));
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraDeTresLivrosDeTresTitulosDiferentes()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(113.4M, livraria.ValorAPagar(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "C", Quantidade = 1 }));
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraDeTresLivrosDeDoisTitulosDiferentesManeira1()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(121.80M, livraria.ValorAPagar(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 2 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 1 }));
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraDeTresLivrosDeDoisTitulosDiferentesManeira2()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(121.80M, livraria.ValorAPagar(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "A", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 1 }));
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraDeQuatroLivrosDeDoisTitulosDiferentesManeira1()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(159.60M, livraria.ValorAPagar(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 2 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 2 }));
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraDeQuatroLivrosDeDoisTitulosDiferentesManeira2()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(159.60m, livraria.ValorAPagar(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "A", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 1 }));
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraDeQuatroLivrosDeTresTitulosDiferentesManeira1()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(155.40M, livraria.ValorAPagar(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 2 },
+				new IntencaoDeCompra { Livro = "C", Quantidade = 1 }));
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraDeQuatroLivrosDeTresTitulosDiferentesManeira2()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(155.40M, livraria.ValorAPagar(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "C", Quantidade = 1 }));
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraDeQuatroLivrosDeQuatroTitulosDiferentes()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(142.80M, livraria.ValorAPagar(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "C", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "D", Quantidade = 1 }));
+		}
+
+		[TestMethod]
+		public void QuandoCalculaACompraDeAcordoComAsInformacoesDoDOJO()
+		{
+			var livraria = new LivrariaHarryPotter();
+			Assert.AreEqual(281.40M, livraria.ValorAPagar(
+				new IntencaoDeCompra { Livro = "A", Quantidade = 2 },
+				new IntencaoDeCompra { Livro = "B", Quantidade = 2 },
+				new IntencaoDeCompra { Livro = "C", Quantidade = 2 },
+				new IntencaoDeCompra { Livro = "D", Quantidade = 1 },
+				new IntencaoDeCompra { Livro = "E", Quantidade = 1 }));
+		}
+	}
+}
