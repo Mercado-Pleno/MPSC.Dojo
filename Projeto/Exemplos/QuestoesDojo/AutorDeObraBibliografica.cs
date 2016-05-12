ffc1ba0217cb9191fe0fa3ceb1ce3a812c997aae
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2016-05-12 05:19:10 +0000

    Commit Do AutorDeObraBibliografica.cs

---
+++ Projeto/Exemplos/QuestoesDojo/AutorDeObraBibliografica.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace MPSC.Library.Exemplos.QuestoesDojo
+{
+	public class AutorDeObraBibliografica
+	{
+
+		public String[] DividirEmNomes(String nome)
+		{
+			return nome.Split(new[] { " ", "\t", "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+		}
+
+		public String SobreNome(String nome)
+		{
+			var nomes = DividirEmNomes(nome);
+			return nomes.Length > 0 ? SobreNomeComposto(nomes, nomes.Length - 1, nomes.Length > 2) : String.Empty;
+		}
+
+		public String PrimeirosNomes(String nome)
+		{
+			var nomes = DividirEmNomes(nome);
+			var sobreNome = DividirEmNomes(SobreNome(nome));
+			return String.Join(" ", nomes.Where(n => !In(n, sobreNome)));
+		}
+
+		public String Formatar(String nome)
+		{
+			var qtdNomes = DividirEmNomes(PrimeirosNomes(nome)).Length;
+			return qtdNomes > 0 ? String.Format("{0}, {1}", SobreNome(nome).ToUpper(), Capitalizar(PrimeirosNomes(nome))) : SobreNome(nome).ToUpper();
+		}
+
+		private String Capitalizar(String nome)
+		{
+			var nomes = DividirEmNomes(nome.ToLower());
+			for (int i = 0; i < nomes.Length; i++)
+			{
+				if (!In(nomes[i], "e", "da", "de", "do", "das", "dos"))
+					nomes[i] = ToFirstUpper(nomes[i]);
+			}
+
+			return String.Join(" ", nomes);
+		}
+
+		private String ToFirstUpper(String nome)
+		{
+			return nome[0].ToString().ToUpper() + nome.Substring(1).ToLower();
+		}
+
+
+		private String SobreNomeComposto(String[] nomes, Int32 posicao, Boolean composto)
+		{
+			var sobreNome = nomes[posicao];
+			if (composto && EhComposto(sobreNome))
+				sobreNome = String.Format("{0} {1}", nomes[posicao - 1], nomes[posicao]);
+			return sobreNome;
+		}
+
+		private Boolean EhComposto(String sobreNome)
+		{
+			return In(sobreNome.ToUpper(), "FILHO", "FILHA", "NETO", "NETA", "SOBRINHO", "SOBRINHA", "JUNIOR");
+		}
+
+		private Boolean In(String sobreNome, params String[] sobrenomes)
+		{
+			return sobrenomes.Contains(sobreNome);
+		}
+
+	}
+}
---
+++ Projeto/[TestesUnitarios]/SolutionTest_v4.0/Exemplos/QuestoesDojo/TestandoAutorDeObraBibliografica.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPSC.Library.Exemplos.QuestoesDojo;
+
+namespace MP.Library.TestesUnitarios.SolutionTest_v4.Exemplos.QuestoesDojo
+{
+	[TestClass]
+	public class TestandoAutorDeObraBibliografica
+	{
+		[TestMethod]
+		public void QuandoTestaDividirEmNomes()
+		{
+			var autorDeObraBibliografica = new AutorDeObraBibliografica();
+
+			Assert.AreEqual(0, autorDeObraBibliografica.DividirEmNomes("").Length);
+			Assert.AreEqual(3, autorDeObraBibliografica.DividirEmNomes("Bruno Nogueira Fernandes").Length);
+		}
+
+		[TestMethod]
+		public void QuandoTestaPrimeirosNomes()
+		{
+			var autorDeObraBibliografica = new AutorDeObraBibliografica();
+
+			Assert.AreEqual("", autorDeObraBibliografica.PrimeirosNomes(""));
+			Assert.AreEqual("Bruno Nogueira", autorDeObraBibliografica.PrimeirosNomes("Bruno Nogueira Fernandes"));
+			Assert.AreEqual("", autorDeObraBibliografica.PrimeirosNomes("Fernandes"));
+			Assert.AreEqual("Antonio", autorDeObraBibliografica.PrimeirosNomes("Antonio Neto"));
+			Assert.AreEqual("Antonio José", autorDeObraBibliografica.PrimeirosNomes("Antonio José Fernandes Neto"));
+		}
+
+		[TestMethod]
+		public void QuandoTestaSobreNome()
+		{
+			var autorDeObraBibliografica = new AutorDeObraBibliografica();
+
+			Assert.AreEqual("", autorDeObraBibliografica.SobreNome(""));
+			Assert.AreEqual("Fernandes", autorDeObraBibliografica.SobreNome("Bruno Nogueira Fernandes"));
+			Assert.AreEqual("Fernandes", autorDeObraBibliografica.SobreNome("Fernandes"));
+			Assert.AreEqual("Neto", autorDeObraBibliografica.SobreNome("Antonio Neto"));
+			Assert.AreEqual("Fernandes Neto", autorDeObraBibliografica.SobreNome("Antonio José Fernandes Neto"));
+		}
+
+		[TestMethod]
+		public void QuandoTestaFormatar()
+		{
+			var autorDeObraBibliografica = new AutorDeObraBibliografica();
+
+			Assert.AreEqual("FERNANDES NETO, Antonio José", autorDeObraBibliografica.Formatar("Antonio José Fernandes Neto"));
+			Assert.AreEqual("FERNANDES NETO, Antonio José", autorDeObraBibliografica.Formatar("ANTONIO JOSÉ FERNANDES NETO"));
+			Assert.AreEqual("FERNANDES NETO, Antonio José", autorDeObraBibliografica.Formatar("antonio josé fernandes neto"));
+
+			Assert.AreEqual("FERNANDES NETO, Antonio José de", autorDeObraBibliografica.Formatar("Antonio José de Fernandes Neto"));
+			Assert.AreEqual("FERNANDES NETO, Antonio José de", autorDeObraBibliografica.Formatar("ANTONIO JOSÉ DE FERNANDES NETO"));
+			Assert.AreEqual("FERNANDES NETO, Antonio José de", autorDeObraBibliografica.Formatar("antonio josé de fernandes neto"));
+
+			Assert.AreEqual("", autorDeObraBibliografica.Formatar(""));
+			Assert.AreEqual("FERNANDES", autorDeObraBibliografica.Formatar("Fernandes"));
+			Assert.AreEqual("FERNANDES, Antonio", autorDeObraBibliografica.Formatar("Antonio Fernandes"));
+			Assert.AreEqual("NETO, Antonio", autorDeObraBibliografica.Formatar("Antonio Neto"));
+			Assert.AreEqual("FERNANDES NETO, Antonio", autorDeObraBibliografica.Formatar("Antonio Fernandes Neto"));
+			Assert.AreEqual("FERNANDES, Antonio José", autorDeObraBibliografica.Formatar("Antonio José Fernandes"));
+			Assert.AreEqual("FERNANDES NETO, Antonio José", autorDeObraBibliografica.Formatar("Antonio José Fernandes Neto"));
+		}
+	}
+}
