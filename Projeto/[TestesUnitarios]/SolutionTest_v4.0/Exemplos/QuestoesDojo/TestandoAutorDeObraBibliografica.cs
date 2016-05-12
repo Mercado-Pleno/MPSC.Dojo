353d6c4a27487e2beb7732154b716d31d4ed5de8
bruno.fernandes <bruno.fernandes@f1676795-5c1f-d048-873b-eaefd5351bb0>
2016-05-12 05:51:39 +0000

    Refatorando classe de AutorDeObraBibliografica

--- Projeto/[TestesUnitarios]/SolutionTest_v4.0/Exemplos/QuestoesDojo/TestandoAutorDeObraBibliografica.cs
+++ Projeto/[TestesUnitarios]/SolutionTest_v4.0/Exemplos/QuestoesDojo/TestandoAutorDeObraBibliografica.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MPSC.Library.Exemplos.QuestoesDojo;
 
 namespace MP.Library.TestesUnitarios.SolutionTest_v4.Exemplos.QuestoesDojo
