29941664b44d05fc3737964c712b1c2672df0037
bruno.fernandes <bruno.fernandes@f1676795-5c1f-d048-873b-eaefd5351bb0>
2016-07-15 23:51:58 +0000

    Job

---
+++ Projeto/[TestesUnitarios]/SolutionTest_v4.0/Exemplos/Job/JobBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MP.Library.TestesUnitarios.SolutionTest_v4.Exemplos.Job
+{
+	public class JobBase<T>
+	{
+		public IJobBase<T> job { get; set; }
+		public void Executar()
+		{
+			var quantidadeDeItensPorLote = job.ObterQuantidadeDeItensPorLote();
+			var dados = job.ObterInformacoes();
+			var lotes = Fatiar(dados, quantidadeDeItensPorLote);
+			foreach (var lote in lotes)
+			{
+				job.ValidarLote(lote);
+				foreach (var item in lote)
+				{
+					if (job.ValidarItem(item))
+						job.ProcessarItem(item);
+
+					job.PosCondicaoItem(item);
+				}
+				job.PosCondicaoLote(lote);
+			}
+		}
+
+		private IEnumerable<IEnumerable<T>> Fatiar(IEnumerable<T> dados, Int32 quantidadeDeItensPorLote)
+		{
+			IEnumerable<T> retorno;
+			for (int i = 0; (retorno = dados.Skip(i * quantidadeDeItensPorLote).Take(quantidadeDeItensPorLote)).Any(); i++)
+				yield return retorno;
+		}
+	}
+
+	public interface IJobBase<T>
+	{
+		Int32 ObterQuantidadeDeItensPorLote();
+		IEnumerable<T> ObterInformacoes();
+		Boolean ValidarLote(IEnumerable<T> lote);
+		Boolean ValidarItem(T n);
+		void ProcessarItem(T n);
+		void PosCondicaoItem(T n);
+		void PosCondicaoLote(IEnumerable<T> lote);
+	}
+}
