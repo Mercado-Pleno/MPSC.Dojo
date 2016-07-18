bc81ebf68b51a8275ad9814ba405ea9a40c0e3cd
bruno.fernandes <bruno.fernandes@f1676795-5c1f-d048-873b-eaefd5351bb0>
2016-07-18 18:27:50 +0000

    Implementação do Exemplo de Cheque Por Extenso

---
+++ Projeto/[TestesUnitarios]/SolutionTest_v4.0/Exemplos/QuestoesDojo/TestandoChequePorExtenso.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPSC.Library.Exemplos.QuestoesDojo;
+
+namespace MP.Library.TestesUnitarios.SolutionTest_v4.Exemplos.QuestoesDojo
+{
+	[TestClass]
+	public class TestandoChequePorExtenso
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var cheque = ChequePorExtenso.Novo(253);
+
+			Assert.AreEqual("duzentos e cinquenta e tres", cheque.Descricao);
+		}
+	}
+}
