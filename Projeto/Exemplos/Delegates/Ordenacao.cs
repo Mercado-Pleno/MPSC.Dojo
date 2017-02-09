cf0cc7b3123170cf778945a488a3ff38b7e2aecf
bruno.fernandes <bruno.fernandes@f1676795-5c1f-d048-873b-eaefd5351bb0>
2017-02-09 20:24:07 +0000

    Implementando exemplo de delegate

---
+++ Projeto/Exemplos/Delegates/Ordenacao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MPSC.Library.Exemplos.Delegates
+{
+	public delegate Boolean Comparar<T>(T a, T b);
+
+	public static class Ordenacao
+	{
+		public static IEnumerable<Tipo> Ordenar<Tipo>(this IEnumerable<Tipo> colecao, Comparar<Tipo> comparar)
+		{
+			var lista = colecao.ToArray();
+
+			for (var i = 0; i < lista.Length - 1; i++)
+			{
+				for (var j = i + 1; j < lista.Length; j++)
+				{
+					if (comparar(lista[i], lista[j]))
+					{
+						Tipo tmp = lista[i];
+						lista[i] = lista[j];
+						lista[j] = tmp;
+					}
+				}
+			}
+
+			return lista;
+		}
+	}
+}
