f23e276f3a10b86fb2a28bee0d13ebff7492f697
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2017-02-10 20:14:40 +0000

    Refatoração no programa Principal para melhorar iteracao com o menu

--- Projeto/Exemplos/Principal.cs
+++ Projeto/Exemplos/Principal.cs
@@ -24,106 +24,105 @@ using System.Linq;
 
 namespace MPSC.Library.Exemplos
 {
-	public interface IExecutavel
-	{
-		void Executar();
-	}
-
 	public static class Principal
 	{
 		public const Char ESC = (char)27;
 		public static void Main(String[] args)
 		{
 			var mainMenu = ObterMenuPrincipal();
-			var itemMenu = mainMenu.MostrarSub();
+			var itemMenu = mainMenu.Exibir();
 			while ((itemMenu != null) && itemMenu.PossuiComando)
 			{
 				itemMenu.Executar();
-				itemMenu = mainMenu.MostrarSub();
+				itemMenu = mainMenu.Exibir();
 			}
 		}
 
-
-		private static ItemMenu ObterMenuPrincipal()
+		private static Menu ObterMenuPrincipal()
 		{
-			return new ItemMenu('0', "",
-				new ItemMenu('1', "Banco de Dados",
-					new ItemMenu('1', "Sql Server",
-						new ItemMenu('1', "Como Pegar As Mensagens De Prints Do Sql Server", new ComoPegarAsMensagensDePrintsDoSqlServer())
+			return new Menu('0', "Principal",
+				new Menu('1', "Banco de Dados",
+					new Menu('1', "Sql Server",
+						new Menu('1', "Como Pegar As Mensagens De Prints Do Sql Server", new ComoPegarAsMensagensDePrintsDoSqlServer())
 					),
-					new ItemMenu('2', "Varias Coisas Com Objetos De Banco De Dados", new VariasCoisasComObjetosDeBancoDeDados())
+					new Menu('2', "Varias Coisas Com Objetos De Banco De Dados", new VariasCoisasComObjetosDeBancoDeDados())
 				),
 
-				new ItemMenu('2', "Controle de Fluxo",
-					new ItemMenu('0', "Processando Ordenação Com Delegate", new ProcessandoOrdenacaoComDelegate()),
-					new ItemMenu('1', "Maquina de Estado", new ValidarTransicoesDeEstado()),
-					new ItemMenu('2', "Saber Quem Instanciou", new SaberQuemInstanciou()),
-					new ItemMenu('3', "Teste De Construtores Estaticos", new TesteDeConstrutoresEstaticos()),
-					new ItemMenu('4', "Diretiva De Pré-Processamento Via Reflection", new DiretivaDePreProcessamentoViaReflection()),
-					new ItemMenu('5', "Definindo Quem Pode Alterar Os Atributos Do Objeto", new DefinindoQuemPodeAlterarOsAtributosDoObjeto()),
-					new ItemMenu('6', "Definindo Quem Pode Alterar Os Atributos Do Objeto Em Tempo Design 1", new DefinindoQuemPodeAlterarOsAtributosDoObjetoEmTempoDesign1()),
-					new ItemMenu('7', "Definindo Quem Pode Alterar Os Atributos Do Objeto Em Tempo Design 2", new DefinindoQuemPodeAlterarOsAtributosDoObjetoEmTempoDesign2()),
-					new ItemMenu('8', "Controlando Se O Programa Esta Sendo Executado", new ControlandoSeOProgramaEstaSendoExecutado()),
-					new ItemMenu('9', "Controlando Recursos Compartilhados", new ControlandoRecursosCompartilhados())
+				new Menu('2', "Controle de Fluxo",
+					new Menu('0', "Processando Ordenação Com Delegate", new ProcessandoOrdenacaoComDelegate()),
+					new Menu('1', "Maquina de Estado", new ValidarTransicoesDeEstado()),
+					new Menu('2', "Saber Quem Instanciou", new SaberQuemInstanciou()),
+					new Menu('3', "Teste De Construtores Estaticos", new TesteDeConstrutoresEstaticos()),
+					new Menu('4', "Diretiva De Pré-Processamento Via Reflection", new DiretivaDePreProcessamentoViaReflection()),
+					new Menu('5', "Definindo Quem Pode Alterar Os Atributos Do Objeto", new DefinindoQuemPodeAlterarOsAtributosDoObjeto()),
+					new Menu('6', "Definindo Quem Pode Alterar Os Atributos Do Objeto Em Tempo Design 1", new DefinindoQuemPodeAlterarOsAtributosDoObjetoEmTempoDesign1()),
+					new Menu('7', "Definindo Quem Pode Alterar Os Atributos Do Objeto Em Tempo Design 2", new DefinindoQuemPodeAlterarOsAtributosDoObjetoEmTempoDesign2()),
+					new Menu('8', "Controlando Se O Programa Esta Sendo Executado", new ControlandoSeOProgramaEstaSendoExecutado()),
+					new Menu('9', "Controlando Recursos Compartilhados", new ControlandoRecursosCompartilhados())
 				),
 
-				new ItemMenu('3', "Reflection",
-					new ItemMenu('1', "Atributos Descritivos Dos Enumerados", new AtributosDescritivosDosEnumerados())
+				new Menu('3', "Reflection",
+					new Menu('1', "Atributos Descritivos Dos Enumerados", new AtributosDescritivosDosEnumerados())
 				),
 
-				new ItemMenu('4', "Medidas",
-					new ItemMenu('1', "Duração de um Intervalo de Tempo de Alta Precisão", new DuracaoIntervaloTempoAltaPrecisao()),
-					new ItemMenu('2', "Performance de Geração de Ids via Sequence vs Tabela", new TestandoPerformanceDeGeracaoDeIdsViaSequenceVsTabela())
+				new Menu('4', "Medidas",
+					new Menu('1', "Duração de um Intervalo de Tempo de Alta Precisão", new DuracaoIntervaloTempoAltaPrecisao()),
+					new Menu('2', "Performance de Geração de Ids via Sequence vs Tabela", new TestandoPerformanceDeGeracaoDeIdsViaSequenceVsTabela())
 				),
 
-				new ItemMenu('5', "Utilidades",
-					new ItemMenu('1', "Separar Lista EMails E Remover Duplicados", new SeparaListaEMailsERemoveDuplicados()),
-					new ItemMenu('2', "Criptografia Com Operador XOR", new CriptografiaComOperadorXOR()),
-					new ItemMenu('3', "Alguma Coisa Com Relatorios usando Linq", new AlgumaCoisaComRelatoriosUsandoLinq()),
-					new ItemMenu('4', "Transformacao De Dados Para DTO", new TransformacaoDeDadosParaDTO()),
-					new ItemMenu('5', "Wake Up On LAN", new ProgramWakeOnLan()),
-					new ItemMenu('6', "Árvore Utópica", new ArvoreUtopica())
+				new Menu('5', "Utilidades",
+					new Menu('1', "Separar Lista EMails E Remover Duplicados", new SeparaListaEMailsERemoveDuplicados()),
+					new Menu('2', "Criptografia Com Operador XOR", new CriptografiaComOperadorXOR()),
+					new Menu('3', "Alguma Coisa Com Relatorios usando Linq", new AlgumaCoisaComRelatoriosUsandoLinq()),
+					new Menu('4', "Transformacao De Dados Para DTO", new TransformacaoDeDadosParaDTO()),
+					new Menu('5', "Wake Up On LAN", new ProgramWakeOnLan()),
+					new Menu('6', "Árvore Utópica", new ArvoreUtopica())
 				),
 
-				new ItemMenu('6', "Design Pattern",
-					new ItemMenu('1', "Strategy - Mini Simulador De Patos", new MiniSimuladorDePatos()),
-					new ItemMenu('2', "Observer - Estação de Monitoramento")
+				new Menu('6', "Design Pattern",
+					new Menu('1', "Strategy - Mini Simulador De Patos", new MiniSimuladorDePatos()),
+					new Menu('2', "Observer - Estação de Monitoramento")
 				),
 
-				new ItemMenu('7', "Métodos de extensão",
-					new ItemMenu('1', "Extrair XML", new ExtensionMethods())
+				new Menu('7', "Métodos de extensão",
+					new Menu('1', "Extrair XML", new ExtensionMethods())
 				),
 
-				new ItemMenu('8', "Questões e Treinamentos de DOJO (On-Line)",
-					new ItemMenu('1', "Gerador de Matriz Espiral", new MatrizEspiralRunner()),
-					new ItemMenu('2', "Numero Romano", new NumeroRomanoRunner()),
-					new ItemMenu('3', "Autor De Obra Bibliográfica", new AutorDeObraBibliograficaRunner()),
-					new ItemMenu('4', "Livraria Do Harry Potter", new LivrariaHarryPotterRunner()),
-					new ItemMenu('5', "Palavras Primas", new PalavrasPrimasRunner()),
-					new ItemMenu('6', "TestaPivot", new TestaPivot())
+				new Menu('8', "Questões e Treinamentos de DOJO (On-Line)",
+					new Menu('1', "Gerador de Matriz Espiral", new MatrizEspiralRunner()),
+					new Menu('2', "Numero Romano", new NumeroRomanoRunner()),
+					new Menu('3', "Autor De Obra Bibliográfica", new AutorDeObraBibliograficaRunner()),
+					new Menu('4', "Livraria Do Harry Potter", new LivrariaHarryPotterRunner()),
+					new Menu('5', "Palavras Primas", new PalavrasPrimasRunner()),
+					new Menu('6', "TestaPivot", new TestaPivot())
 				),
 
-				new ItemMenu(ESC, "Sair")
+				new Menu(ESC, "Sair")
 			);
 		}
 	}
+	
+	public interface IExecutavel
+	{
+		void Executar();
+	}
 
-	public class ItemMenu
+	public class Menu
 	{
 		private readonly Char Codigo;
 		private readonly String Descricao;
-		private readonly ListaMenu SubMenus;
+		private readonly Menus SubMenus;
 		private readonly IExecutavel Comando;
-		public ItemMenu Parent { get; internal set; }
+		public Menu Parent { get; internal set; }
 		public Boolean PossuiComando { get { return Comando != null; } }
 		public Boolean PossuiSubMenus { get { return SubMenus.Count > 0; } }
 
 		#region //Contrutores
-		public ItemMenu(Char codigo, String descricao) { Codigo = codigo; Descricao = descricao; SubMenus = new ListaMenu(this); }
+		public Menu(Char codigo, String descricao) { Codigo = codigo; Descricao = descricao; SubMenus = new Menus(this); }
 
-		public ItemMenu(Char codigo, String descricao, IExecutavel comando) : this(codigo, descricao) { Comando = comando; }
+		public Menu(Char codigo, String descricao, IExecutavel comando) : this(codigo, descricao) { Comando = comando; }
 
-		public ItemMenu(Char codigo, String descricao, params ItemMenu[] menus) : this(codigo, descricao) { SubMenus.AddRange(menus); SubMenus.ForEach(m => m.Parent = this); }
+		public Menu(Char codigo, String descricao, params Menu[] menus) : this(codigo, descricao) { SubMenus.Add(menus); }
 
 		#endregion //Contrutores
 
@@ -132,65 +131,82 @@ namespace MPSC.Library.Exemplos
 			return Codigo == codigo;
 		}
 
-		public void Mostrar()
+		internal void ExibirItem()
 		{
 			Console.WriteLine(Codigo + " - " + Descricao);
 		}
 
-		internal ItemMenu MostrarSub()
+		internal Menu Exibir()
 		{
-			return SubMenus.Mostrar();
+			return SubMenus.Exibir();
 		}
 
 		public void Executar()
 		{
 			Comando.Executar();
+			AguardarEnterOuEsc("\r\nPressione {Enter} Ou {Esc} para voltar ao menu");
+		}
+
+		private void AguardarEnterOuEsc(String mensagem)
+		{
+			Console.WriteLine(mensagem);
 			var key = Console.ReadKey();
 			while ((key.Key != ConsoleKey.Escape) && (key.Key != ConsoleKey.Enter))
+			{
+				LimparUltimoCaracter();
 				key = Console.ReadKey();
+			}
+		}
+
+		private void LimparUltimoCaracter()
+		{
+			Console.CursorLeft--;
+			Console.Write(" ");
+			Console.CursorLeft--;
 		}
 	}
 
-	public class ListaMenu : List<ItemMenu>
+	public class Menus : List<Menu>
 	{
-		public readonly ItemMenu Parent;
-		public ListaMenu(ItemMenu parent, params ItemMenu[] menus) : base(menus) { Parent = parent; ForEach(m => m.Parent = parent); }
+		private readonly Menu Parent;
 
-		private void InternalShow(String primeiraOpcao)
+		public Menus(Menu parent) { Parent = parent; }
+
+		public void Add(IEnumerable<Menu> menus)
+		{
+			AddRange(menus);
+			ForEach(m => m.Parent = Parent);
+		}
+
+		private void Exibir(String primeiraOpcao)
 		{
 			Console.Clear();
 			Console.WriteLine(primeiraOpcao);
 			foreach (var item in this)
-				item.Mostrar();
+				item.ExibirItem();
 		}
 
-		public ItemMenu Mostrar()
+		public Menu Exibir()
 		{
-			InternalShow("Selecione\r\n");
-
-			return EsperarUsuario();
-		}
-
-		private ItemMenu EsperarUsuario()
-		{
-			var itemMenu = Avaliar();
+			Exibir("Selecione\r\n");
+			var itemMenu = EsperarUsuarioEscolherOpcaoDoMenu();
 			while (itemMenu == null)
 			{
-				InternalShow("Opção inválida\r\nSelecione\r\n");
-				itemMenu = Avaliar();
+				Exibir("Opção inválida\r\nSelecione\r\n");
+				itemMenu = EsperarUsuarioEscolherOpcaoDoMenu();
 			}
 
 			return itemMenu;
 		}
 
-		private ItemMenu Avaliar()
+		private Menu EsperarUsuarioEscolherOpcaoDoMenu()
 		{
 			var codigo = Console.ReadKey().KeyChar;
 
 			var itemMenu = this.FirstOrDefault(m => m.CodigoEhIgual(codigo));
 
 			while ((itemMenu != null) && itemMenu.PossuiSubMenus)
-				itemMenu = itemMenu.MostrarSub();
+				itemMenu = itemMenu.Exibir();
 
 			return (itemMenu == null) && (codigo == Principal.ESC) ? Parent.Parent : itemMenu;
 		}
