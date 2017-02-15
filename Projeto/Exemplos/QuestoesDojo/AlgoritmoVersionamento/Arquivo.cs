b4bc181cce87c0123595cf020dc0067fc65d7477
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2017-02-15 21:49:34 +0000

    Implementando AlgoritmoVersionamento

---
+++ Projeto/Exemplos/QuestoesDojo/AlgoritmoVersionamento/Arquivo.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPSC.Library.Exemplos.QuestoesDojo.AlgoritmoVersionamento
+{
+	public class Versionador
+	{
+		private readonly Arquivo versaoInicial = new Arquivo();
+		public readonly List<Modificacao> _modificacoes = new List<Modificacao>();
+		public Int32 Versao { get { return _modificacoes.Count(); } }
+
+		public void Versionar(Arquivo arquivo)
+		{
+			if (versaoInicial.EstaVazio)
+				versaoInicial.Adicionar(arquivo.Conteudo);
+			else
+			{
+				var arquivoAtual = ObterArquivo();
+				var modificacoes = comparar(arquivoAtual, arquivo).OrderBy(m => m.Prioridade).ThenBy(m => m.Linha.Posicao).ToArray();
+				_modificacoes.AddRange(modificacoes);
+			}
+		}
+
+		public Arquivo ObterArquivo()
+		{
+			var arquivo = new Arquivo(versaoInicial.Linhas);
+			_modificacoes.ForEach(m => m.Aplicar(arquivo));
+			return arquivo;
+		}
+
+		private IEnumerable<Modificacao> comparar(Arquivo arquivoA, Arquivo arquivoB)
+		{
+			foreach (var linhaA in arquivoA.Linhas)
+			{
+				var linhaB = arquivoB.Linhas.FirstOrDefault(l => l.Conteudo == linhaA.Conteudo);
+				if (linhaB == null)
+					yield return new Exclusao(linhaA);
+			}
+
+			foreach (var linhaB in arquivoB.Linhas)
+			{
+				var linhaA = arquivoA.Linhas.FirstOrDefault(l => l.Conteudo == linhaB.Conteudo);
+				if (linhaA == null)
+					yield return new Insercao(linhaB);
+				else if (linhaB.Posicao != linhaA.Posicao)
+					yield return new Adicao(linhaB);
+			}
+		}
+
+	}
+
+	public static class Extension
+	{
+		public static void ForEach<T>(this IEnumerable<T> source, Action<T> acao)
+		{
+			foreach (var item in source)
+				acao(item);
+		}
+	}
+
+	public class Arquivo
+	{
+		private readonly List<Linha> _linhas = new List<Linha>();
+		public Arquivo(IEnumerable<Linha> linhas) { Adicionar(linhas); }
+		public Arquivo(String conteudo) { Adicionar(conteudo); }
+		public Arquivo(params String[] linhas) { Adicionar(linhas); }
+
+		public void Adicionar(String conteudo)
+		{
+			Adicionar(conteudo.Split(new[] { "\r\n" }, StringSplitOptions.None));
+		}
+
+		private void Adicionar(IEnumerable<String> linhas)
+		{
+			Adicionar(linhas.Select((l, i) => new Linha(l, i + 1)));
+		}
+
+		public void Adicionar(IEnumerable<Linha> linhas)
+		{
+			linhas.ForEach(Inserir);
+		}
+
+		public void Inserir(Linha linha)
+		{
+			var posicaoDaProximaLinha = PosicaoDaUltimaLinha + 1;
+			if (linha.Posicao < posicaoDaProximaLinha)
+				_linhas.Where(l => l.Posicao >= linha.Posicao).ForEach(l => l.Avancar());
+			else if (linha.Posicao > posicaoDaProximaLinha)
+			{
+				var quantidade = linha.Posicao - posicaoDaProximaLinha;
+				var linhasEmBranco = Enumerable.Range(posicaoDaProximaLinha, quantidade);
+				_linhas.AddRange(linhasEmBranco.Select(p => new Linha(String.Empty, p)));
+			}
+			_linhas.Add(linha);
+		}
+
+		public void Atualizar(Linha linha)
+		{
+			Remover(linha);
+			_linhas.Add(linha);
+		}
+
+		public void Remover(Linha linha)
+		{
+			var line = _linhas.FirstOrDefault(l => l.Posicao == linha.Posicao);
+			if (line != null)
+			{
+				_linhas.Where(l => l.Posicao > line.Posicao).ForEach(l => l.Retroceder());
+				_linhas.Remove(line);
+			}
+		}
+
+		private Int32 PosicaoDaUltimaLinha { get { return _linhas.Any() ? _linhas.Max(l => l.Posicao) : 0; } }
+
+		public String Conteudo { get { return String.Join("\r\n", Linhas.Select(l => l.Conteudo)); } }
+
+		public Boolean EstaVazio { get { return !_linhas.Any(); } }
+
+		public IEnumerable<Linha> Linhas { get { return _linhas.OrderBy(l => l.Posicao); } }
+
+		public override String ToString()
+		{
+			return Conteudo;
+		}
+	}
+
+	public class Linha
+	{
+		public readonly String Conteudo;
+		private Int32 _posicao;
+
+		public Int32 Posicao { get { return _posicao; } }
+
+		public Linha(String conteudo, Int32 posicao)
+		{
+			Conteudo = conteudo;
+			_posicao = posicao;
+		}
+
+		public void Avancar()
+		{
+			_posicao++;
+		}
+		public void Retroceder()
+		{
+			_posicao--;
+		}
+		public override String ToString()
+		{
+			return Conteudo;
+		}
+	}
+
+	public abstract class Modificacao
+	{
+		public int Prioridade { get; private set; }
+		public Linha Linha { get; private set; }
+
+		public Modificacao(Linha linha, Int32 prioridade) { Linha = new Linha(linha.Conteudo, linha.Posicao); Prioridade = prioridade; }
+		public abstract void Aplicar(Arquivo arquivo);
+
+		public override String ToString()
+		{
+			return String.Format("{0} {1}", GetType().Name, Linha.ToString());
+		}
+	}
+	public class Exclusao : Modificacao
+	{
+		public Exclusao(Linha linha) : base(linha, 1) { }
+		public override void Aplicar(Arquivo arquivo)
+		{
+			arquivo.Remover(Linha);
+		}
+	}
+
+	public class Adicao : Modificacao
+	{
+		public Adicao(Linha linha) : base(linha, 2) { }
+
+		public override void Aplicar(Arquivo arquivo)
+		{
+			arquivo.Atualizar(Linha);
+		}
+	}
+
+
+	public class Insercao : Modificacao
+	{
+		public Insercao(Linha linha) : base(linha, 3) { }
+		public override void Aplicar(Arquivo arquivo)
+		{
+			arquivo.Inserir(Linha);
+		}
+	}
+}
---
+++ Projeto/[TestesUnitarios]/SolutionTest_v4.0/Exemplos/QuestoesDojo/AlgoritmoVersionamento/TestandoVersionador.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPSC.Library.Exemplos.QuestoesDojo.AlgoritmoVersionamento;
+using System;
+
+namespace MP.Library.TestesUnitarios.SolutionTest_v4.Exemplos.QuestoesDojo.AlgoritmoVersionamento
+{
+	[TestClass]
+	public class TestandoVersionador
+	{
+		private Versionador versionador = new Versionador();
+
+		[TestInitialize]
+		public void InicializarTeste()
+		{
+			var arquivo = new Arquivo("Bruno");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(0, versionador.Versao);
+		}
+
+		[TestMethod]
+		public void Quando_Versiona_O_Mesmo_Arquivo_Nao_Gera_Alteracoes()
+		{
+			var arquivo = new Arquivo("Bruno");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(0, versionador.Versao);
+		}
+
+		[TestMethod]
+		public void Quando_Versiona_Arquivo_Com1_Alteracao_OVersionador_DeveSer_Versao1()
+		{
+			var arquivo = new Arquivo("Bruno", "Fernandes");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(1, versionador.Versao);
+
+			var arquivoVersionado = versionador.ObterArquivo();
+			Assert.AreEqual(arquivo.Conteudo, arquivoVersionado.Conteudo);
+		}
+
+		[TestMethod]
+		public void Quando_Versiona_Arquivo_Com2_Alteracao_OVersionador_DeveSer_Versao2()
+		{
+			var arquivo = new Arquivo("Bruno", "Fernandes");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(1, versionador.Versao);
+
+			arquivo = new Arquivo("Bruno", "Nogueira", "Fernandes");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(3, versionador.Versao);
+
+			var arquivoVersionado = versionador.ObterArquivo();
+			Assert.AreEqual(arquivo.Conteudo, arquivoVersionado.Conteudo);
+		}
+
+		[TestMethod]
+		public void Quando_Versiona_Arquivo_Com5_Alteracao_OVersionador_DeveSer_Versao5()
+		{
+			var arquivo = new Arquivo("Bruno", "Fernandes");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(1, versionador.Versao);
+
+			arquivo = new Arquivo("Bruno", "Nogueira", "Fernandes");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(3, versionador.Versao);
+
+			arquivo = new Arquivo("Bruno", "Nogueira", "Fernandes", "Luani", "Santos");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(5, versionador.Versao);
+
+			var arquivoVersionado = versionador.ObterArquivo();
+			Assert.AreEqual(arquivo.Conteudo, arquivoVersionado.Conteudo);
+		}
+
+		[TestMethod]
+		public void Quando_Versiona_Arquivo_Com6_Alteracao_OVersionador_DeveSer_Versao6()
+		{
+			var arquivo = new Arquivo("Bruno", "Fernandes");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(1, versionador.Versao);
+
+			arquivo = new Arquivo("Bruno", "Nogueira", "Fernandes");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(3, versionador.Versao);
+
+			arquivo = new Arquivo("Bruno", "Nogueira", "Fernandes", "Luani", "Santos");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(5, versionador.Versao);
+
+			arquivo = new Arquivo("Bruno", "Fernandes", "Luani", "Santos");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(6, versionador.Versao);
+
+			var arquivoVersionado = versionador.ObterArquivo();
+			Assert.AreEqual(arquivo.Conteudo, arquivoVersionado.Conteudo);
+		}
+
+		[TestMethod]
+		public void Quando_Versiona_Arquivo_Com7_Alteracao_OVersionador_DeveSer_Versao7()
+		{
+			var arquivo = new Arquivo("Bruno", "Fernandes");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(1, versionador.Versao);
+
+			arquivo = new Arquivo("Bruno", "Nogueira", "Fernandes");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(3, versionador.Versao);
+
+			arquivo = new Arquivo("Bruno", "Nogueira", "Fernandes", "Luani", "Santos");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(5, versionador.Versao);
+
+			arquivo = new Arquivo("Bruno", "Fernandes", "Luani", "Santos", "Papae");
+			versionador.Versionar(arquivo);
+			Assert.AreEqual(7, versionador.Versao);
+
+			var arquivoVersionado = versionador.ObterArquivo();
+			Assert.AreEqual(arquivo.Conteudo, arquivoVersionado.Conteudo);
+		}
+	}
+
+
+	[TestClass]
+	public class TestandoArquivo
+	{
+		[TestMethod]
+		public void Quando_Cria_Um_Arquivo_Seu_Conteudo_Deve_Ser_Vazio()
+		{
+			var arquivo = new Arquivo();
+
+			Assert.AreEqual(String.Empty, arquivo.Conteudo);
+		}
+
+		[TestMethod]
+		public void Quando_Cria_Um_Arquivo_E_Adiciona_UmaLinha_Seu_Conteudo_Deve_Ser_O_Conteudo_DaLinha()
+		{
+			var arquivo = new Arquivo();
+			var linha = new Linha("Bruno", 1);
+			arquivo.Atualizar(linha);
+
+			Assert.AreEqual(linha.Conteudo, arquivo.Conteudo);
+		}
+
+		[TestMethod]
+		public void Quando_Cria_Um_Arquivo_E_Adiciona_Uma_Linha3_Seu_Conteudo_Deve_Ser_2Linhas_Em_Branco_e_Conteudo_DaLinha()
+		{
+			var arquivo = new Arquivo();
+			var linha = new Linha("Bruno", 3);
+			arquivo.Inserir(linha);
+
+			Assert.AreEqual("\r\n\r\n" + linha.Conteudo, arquivo.Conteudo);
+		}
+
+		[TestMethod]
+		public void Quando_Cria_Um_Arquivo_E_Adiciona_Linha3_Linha1_Seu_Conteudo_Deve_Ser_de4Linhas()
+		{
+			var arquivo = new Arquivo();
+			arquivo.Inserir(new Linha("Fernandes", 3));
+			arquivo.Inserir(new Linha("Bruno", 1));
+
+			Assert.AreEqual("Bruno\r\n\r\n\r\nFernandes", arquivo.Conteudo);
+		}
+
+		[TestMethod]
+		public void Quando_Cria_Um_Arquivo_E_Adiciona_Linha3_Linha1_ERemoveLinha2_Seu_Conteudo_Deve_Ser_de3Linhas()
+		{
+			var arquivo = new Arquivo();
+			arquivo.Atualizar(new Linha("Fernandes", 3));
+			arquivo.Atualizar(new Linha("Bruno", 1));
+			arquivo.Remover(new Linha(String.Empty, 2));
+
+			Assert.AreEqual("Bruno\r\nFernandes", arquivo.Conteudo);
+		}
+
+		[TestMethod]
+		public void Quando_Cria_Um_Arquivo_Com_ConteudoNo_Construtor_O_Conteudo_Deve_Ser_deIgual_Ao_Informado()
+		{
+			var conteudoOriginal = "Bruno\r\n\r\n\r\nFernandes";
+			var arquivo = new Arquivo(conteudoOriginal);
+
+			Assert.AreEqual(conteudoOriginal, arquivo.Conteudo);
+		}
+
+	}
+}
