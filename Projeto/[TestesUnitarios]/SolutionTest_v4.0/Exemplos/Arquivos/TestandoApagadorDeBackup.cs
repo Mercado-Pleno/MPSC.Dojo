428ababc77e0181a5a0c76ecd21a520361da365a
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2017-06-26 21:53:30 +0000

    limpa backup antigo

---
+++ Projeto/[TestesUnitarios]/SolutionTest_v4.0/Exemplos/Arquivos/TestandoApagadorDeBackup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MPSC.Einstein;
+using System.IO;
+using System.Linq;
+
+namespace MP.Library.TestesUnitarios.SolutionTest_v4.Exemplos.Arquivos
+{
+	[TestClass]
+	public class TestandoApagadorDeBackup
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var regex = ApagadorDeBackup.cValidador;
+			Assert.IsTrue(regex.IsMatch("2016.06.01_06.00.rar"));
+			Assert.IsTrue(regex.IsMatch("2016.05.01_06.00.rar"));
+			Assert.IsTrue(regex.IsMatch("2016.06.01_05.00.rar"));
+			Assert.IsTrue(regex.IsMatch("2016.06.01_05.01.rar"));
+			Assert.IsFalse(regex.IsMatch("2016.06.02_05.01.rar"));
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			var fileInfo = new FileInfo(@"D:\Prj\MP\DOJO.Projeto\Bin\arquivos\MPSC.Einstein.txt");
+
+			var arquivos = File.ReadAllLines(fileInfo.FullName)
+				.Select(l => new FileInfo(Path.Combine(fileInfo.Directory.FullName, l.Substring(3))))
+				.ToArray();
+
+			foreach (var arquivo in arquivos)
+			{
+				if (!arquivo.Directory.Exists)
+					arquivo.Directory.Create();
+
+				if (!arquivo.Exists)
+					File.WriteAllText(arquivo.FullName, "arquivoFake.rar");
+			}
+		}
+
+		[TestMethod]
+		public void TestMethod3()
+		{
+			new ApagadorDeBackup().Apagar();
+		}
+	}
+}
